cb1383174ac4400c806e48b2b587426689e46a29
ncarlsonMHN <ncarlson@mhnchicago.org>
2025-08-05 16:28:33 -0500

    added attractions to pipeline

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -5,14 +5,17 @@ use schema DEVOPS_WITH_SNOWFLAKE_PROD.gold;
 -- declarative target table of pipeline
 create or alter table vacation_spots (
     city varchar
-  , airport varchar
-  , co2_emissions_kg_per_person float
-  , punctual_pct float
-  , avg_temperature_air_f float
-  , avg_relative_humidity_pct float
-  , avg_cloud_cover_pct float
-  , precipitation_probability_pct float
-  -- STEP 5: INSERT CHANGES HERE
+    , airport varchar
+    , co2_emissions_kg_per_person float
+    , punctual_pct float
+    , avg_temperature_air_f float
+    , avg_relative_humidity_pct float
+    , avg_cloud_cover_pct float
+    , precipitation_probability_pct float
+    -- STEP 5: INSERT CHANGES HERE
+    , aquarium_cnt int
+    , zoo_cnt int
+    , korean_restaurant_cnt int
 ) data_retention_time_in_days = 1;
 
 
@@ -27,29 +30,37 @@ create or alter task vacation_spots_update
     join silver.weather_joined_with_major_cities city 
         on city.geo_name = flight.arrival_city
     -- STEP 5: INSERT CHANGES HERE
+    join silver.attractions att 
+        on att.geo_id = city.geo_id
   ) as harmonized_vacation_spots 
     ON vacation_spots.city = harmonized_vacation_spots.arrival_city 
     and vacation_spots.airport = harmonized_vacation_spots.arrival_airport
   WHEN MATCHED THEN
     UPDATE SET
         vacation_spots.co2_emissions_kg_per_person = harmonized_vacation_spots.co2_emissions_kg_per_person
-      , vacation_spots.punctual_pct = harmonized_vacation_spots.punctual_pct
-      , vacation_spots.avg_temperature_air_f = harmonized_vacation_spots.avg_temperature_air_f
-      , vacation_spots.avg_relative_humidity_pct = harmonized_vacation_spots.avg_relative_humidity_pct
-      , vacation_spots.avg_cloud_cover_pct = harmonized_vacation_spots.avg_cloud_cover_pct
-      , vacation_spots.precipitation_probability_pct = harmonized_vacation_spots.precipitation_probability_pct
-      -- STEP 5: INSERT CHANGES HERE
+        , vacation_spots.punctual_pct = harmonized_vacation_spots.punctual_pct
+        , vacation_spots.avg_temperature_air_f = harmonized_vacation_spots.avg_temperature_air_f
+        , vacation_spots.avg_relative_humidity_pct = harmonized_vacation_spots.avg_relative_humidity_pct
+        , vacation_spots.avg_cloud_cover_pct = harmonized_vacation_spots.avg_cloud_cover_pct
+        , vacation_spots.precipitation_probability_pct = harmonized_vacation_spots.precipitation_probability_pct
+        -- STEP 5: INSERT CHANGES HERE
+        , vacation_spots.aquarium_cnt = harmonized_vacation_spots.aquarium_cnt
+        , vacation_spots.zoo_cnt = harmonized_vacation_spots.zoo_cnt
+        , vacation_spots.korean_restaurant_cnt = harmonized_vacation_spots.korean_restaurant_cnt
   WHEN NOT MATCHED THEN 
     INSERT VALUES (
         harmonized_vacation_spots.arrival_city
-      , harmonized_vacation_spots.arrival_airport
-      , harmonized_vacation_spots.co2_emissions_kg_per_person
-      , harmonized_vacation_spots.punctual_pct
-      , harmonized_vacation_spots.avg_temperature_air_f
-      , harmonized_vacation_spots.avg_relative_humidity_pct
-      , harmonized_vacation_spots.avg_cloud_cover_pct
-      , harmonized_vacation_spots.precipitation_probability_pct
-      -- STEP 5: INSERT CHANGES HERE
+        , harmonized_vacation_spots.arrival_airport
+        , harmonized_vacation_spots.co2_emissions_kg_per_person
+        , harmonized_vacation_spots.punctual_pct
+        , harmonized_vacation_spots.avg_temperature_air_f
+        , harmonized_vacation_spots.avg_relative_humidity_pct
+        , harmonized_vacation_spots.avg_cloud_cover_pct
+        , harmonized_vacation_spots.precipitation_probability_pct
+        -- STEP 5: INSERT CHANGES HERE
+        , harmonized_vacation_spots.aquarium_cnt
+        , harmonized_vacation_spots.zoo_cnt
+        , harmonized_vacation_spots.korean_restaurant_cnt
     );
 
 -- task to select perfect vacation spot and send email with vacation plan
@@ -66,8 +77,9 @@ create or alter task email_notification
           and punctual_pct >= 50
           and avg_temperature_air_f >= 70
           -- STEP 5: INSERT CHANGES HERE
+          and korean_restaurant_cnt > 0
+          and (zoo_cnt > 0 or aquarium_cnt > 0)
         limit 10);
-
 
       if (:options = '[]') then
         CALL SYSTEM$SEND_EMAIL(
